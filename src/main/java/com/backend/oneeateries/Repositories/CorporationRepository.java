aa42b284d77f00e8c1d7c1b8e9b9259aa124b63e
Chirag-Reshi <73495074+Chirag-Reshi@users.noreply.github.com>
2021-06-09 17:21:25 +0530

    Add files via upload

---
+++ src/main/java/com/backend/oneeateries/Repositories/CorporationRepository.java
@@ -0,0 +1,25 @@
+package com.backend.oneeateries.Repositories;
+
+import com.backend.oneeateries.Model.Corporation;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.bson.types.ObjectId;
+
+@Service
+public class CorporationRepository {
+
+
+    //List of Corporation
+    public List<Corporation> corporationList = new ArrayList<Corporation>();
+    //Adding Corporation to the List
+    public String addCorporation(Corporation corporation){
+        //a unique 24-byte hexadecimal string
+        corporation.setCorporationID(ObjectId.get().toHexString());
+        return corporation.toString();
+    }
+
+}
